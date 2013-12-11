195c9c1f9a92dee1bd10b934e8622edbf91c088e
Gianni Hong <gianni.hong@efwuhn.com>
2013-12-11 12:05:43 +0800

    Rename the GAE logging file

--- Eclipse/JavaUtilLoggingLogger.java
+++
@@ -1,9 +0,0 @@
-import java.util.logging.Logger;
-
-	private static final Logger log = Logger.getLogger(MyServlet.class.getName());
-
-		log.info("An informational message.");
-
-        log.warning("A warning message.");
-
-        log.severe("An error message.");
