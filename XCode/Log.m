a7ad9da67d39819fa154232e8b39f36d7a9629b8
Gianni Hong <gianni.hong@efwuhn.com>
2014-02-12 16:44:26 +0800

    Add view's frame & class name logging

---
+++ XCode/Log.m
@@ -0,0 +1,8 @@
+// Runtime Function
+NSLog(@"Triggered Function: [%s]", __PRETTY_FUNCTION__);
+
+// Runtime Class Name
+NSLog(@"Class Name: %@", [<#reference#> class]);
+
+// View Frame
+NSLog(@"View's Frame: %@", NSStringFromCGRect(<#view.frame#>));
